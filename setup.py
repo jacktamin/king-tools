87724ba8afaec6d5153b31377e63c0f7238b82c3
Ahmad <ahmad@kali>
2021-05-30 18:21:09 +0100

    first

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+packages = ['king_libs',
+'king_libs/king_tools',
+'king_libs/number_to_word_project']
+
+
+
+
+
+
+
+
+setup(name= "king_libs",
+
+version = "0.0.5",
+
+description = "This is code with king package",
+long_description = "This Are just Some Test Libs",
+author = "Ahmad Dehghani",
+author_email = 'ahd76money@gmail.com',
+license='MIT',
+url = 'https://github.com/jacktamin/king_libs'
+
+download_url = 'https://github.com/jacktamin/king_libs/archive/pypi-0_1_3.tar.gz',
+  keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
+packages = packages
+,
+
+install_requires = [])
